c3834ceebc441d84ec9dd4227eef8095696ecf70
EternalVigil <theeternalvigil@gmail.com>
2016-10-23 17:30:26 -0700

    initial commit

---
+++ Assets/javascript/app.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+var QandA = {
+	question1: ["What is your quest?", "to praise the sun", "to provide glory to King Arthur, King of the Britons", "to find the holy grail"],
+	question1Answer: 3,
+	
+};
+
+function giveAnswer(){
+	
+}
+
+function giveQuestion(){
+	
+}
+
+giveQuestion();
