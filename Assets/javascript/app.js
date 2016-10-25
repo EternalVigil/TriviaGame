37d8b533bd083aa22938ac8b802eca009b20b79d
EternalVigil <theeternalvigil@gmail.com>
2016-10-24 21:48:34 -0700

    javascript additions

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,17 +1,52 @@
 // JavaScript Document
+window.onload = function(){
+};
 var QandA = {
 	question1: ["What is your quest?", "to praise the sun", "to provide glory to King Arthur, King of the Britons", "to find the holy grail"],
 	question1Answer: 3,
-	
 };
-var maxTime = 15000;
-function giveAnswer(){
+
+$("#answer1").on("click", function(){
 	
+});
+$("#answer2").on("click", function(){
+	
+});
+$("#answer3").on("click", function(){
+	
+});
+$("#answer4").on("click", function(){
+	
+});
+
+
+var timeLimit = 15000;
+var timeElapsed = 0;
+
+function giveAnswer(){
+	alert("Sorry, you ran out of time.<br\>The answer was ");
 }
 
 function giveQuestion(){
-	setTimeout(giveAnswer, maxTime);
-	$("#timeLeft").html("00:" + maxTime);
+	timeElasped++;
+	var covertTime = timeConvert(timeElapsed);
+	$("#timeLeft").html(timeElasped);
+}
+
+function timeConvert (time){
+	var minutes = Math.floor(time/60);
+	var seconds = time - (minutes * 60);
+	if (seconds < 10){
+		seconds = "0" + seconds;
+	}
+	else if (minutes === 0){
+		minutes = "0" + minutes;
+	}
+return minutes + ":" + seconds;
+}
+
+function resetTimer(){
+	timeElasped = 0;
 }
 
 giveQuestion();
